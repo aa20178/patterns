bcc2f37a9ffd691d81ec981a9c1674d91055d17d
AA <afifiabdou@gmail.com>
2020-01-03 17:06:49 +0100

    tp5 maj

--- tp5/question1/Ensemble.java
+++ tp5/question1/Ensemble.java
@@ -2,46 +2,81 @@ package question1;
 
 import java.util.*;
 
-public class Ensemble<T> extends AbstractSet<T> {
+public class Ensemble<T> extends AbstractSet<T> 
+{
 
-	protected java.util.Vector<T> table = new java.util.Vector<T>();
+    protected java.util.Vector<T> table = new java.util.Vector<T>();
 
-	public int size() {
-		return table.size();
-	}
+    public int size() 
+    {
+        return table.size();
+    }
 
-	public Iterator<T> iterator() {
-		return table.iterator();
-	}
+    public Iterator<T> iterator() 
+    {
+        return table.iterator();
+    }
 
-	public boolean add(T t) {
-		// à compléter pour la question1-1
+    public boolean add(T t)
+    {
+        if (table.contains(t))
+            return false;
 
-		return false;
-	}
+        else
+        {
+            table.add(t);
+            return true;
+        }
+    
+    }
 
-	public Ensemble<T> union(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
 
-		return null;
-	}
+    public Ensemble<T> union(Ensemble<? extends T> e)
+    {
+        if (!e.isEmpty())
+        {
+            Ensemble<T> unionE = new Ensemble<T>();
+            unionE.addAll(this);
+            unionE.addAll(e);
+            return unionE;
+        }
+        else return this; // la réunion avec un ensemble vide redonne l'ensemble non vide
 
-	public Ensemble<T> inter(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
+    }
 
-		return null;
-	}
+    public Ensemble<T> inter(Ensemble<? extends T> e)
+    {
+        if (!e.isEmpty())
+        {
+            Ensemble<T> interE = new Ensemble<T>();
+            interE.addAll(this);
+            interE.retainAll(e);
+            return interE;
+        }
+        else return new Ensemble<T>(); // l'intersection avec un ensemble vide est un ensebmble vide*/
+    }
 
-	public Ensemble<T> diff(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
+    public Ensemble<T> diff(Ensemble<? extends T> e) {
+        if (!e.isEmpty())
+        {
+            Ensemble<T> interE = new Ensemble<T>();
+            interE.addAll(this);
+            interE.addAll(e);
+            interE.removeAll(e);
+            return interE;
+        }
+        else return this;
+    }
 
-		return null;
-	}
+    Ensemble<T> diffSym(Ensemble<? extends T> e) 
+    {
+        if (!e.isEmpty())
+        {
+            return this.union(e).diff(this.inter(e));
 
-	Ensemble<T> diffSym(Ensemble<? extends T> e) {
-		// à compléter pour la question1-2
+        }
+        else return this;
+    }
+    }
+    
 
-		return null;
-	}
-	
-}
--- tp5/question1/EnsembleTest.java
+++ tp5/question1/EnsembleTest.java
@@ -2,21 +2,118 @@ package question1;
 
 public class EnsembleTest extends junit.framework.TestCase {
 
-	public void testUnion() {
-		question1.Ensemble<Integer> e1, e2;
-		e1 = new question1.Ensemble<Integer>();
-		assertEquals(true, e1.add(2));
-		assertEquals(true, e1.add(3));
+    public void testAdd()
+    {
+        question1.Ensemble<Integer> e1, e2;
+        e1 = new question1.Ensemble<Integer>();
+        assertEquals(true, e1.add(2));
+        assertEquals(true, e1.add(3));
+        assertEquals(false, e1.add(2));
+        assertEquals(false, e1.add(3));
 
-		e2 = new question1.Ensemble<Integer>();
-		assertEquals(true, e2.add(3));
-		assertEquals(true, e2.add(4));
 
-		question1.Ensemble<Integer> union = e1.union(e2);
-		assertEquals(3, union.size());
-		assertTrue(union.contains(2));
-		assertTrue(union.contains(3));
-		assertTrue(union.contains(4));
-	}
-	
-}
+        e2 = new question1.Ensemble<Integer>();
+        assertEquals(true, e2.add(3));
+        assertEquals(true, e2.add(4));
+    }
+
+    public void testUnion() {
+        question1.Ensemble<Integer> e1, e2;
+        e1 = new question1.Ensemble<Integer>();
+                e2 = new question1.Ensemble<Integer>();
+        e1.add(1);
+        e1.add(3);
+        e1.add(2);
+
+        e2.add(3);
+        e2.add(4);
+
+
+        question1.Ensemble<Integer> union = e1.union(e2);
+        assertEquals(4, union.size());
+        assertTrue(union.contains(1));
+        assertTrue(union.contains(2));
+        assertTrue(union.contains(3));
+        assertTrue(union.contains(4));
+    }
+
+    public void testInter() {
+        question1.Ensemble<Integer> e1, e2;
+        e1 = new question1.Ensemble<Integer>();
+        e2 = new question1.Ensemble<Integer>();
+
+        e1.add(2);
+        e1.add(3);
+
+        e2.add(3);
+        e2.add(4);
+
+        question1.Ensemble<Integer> inter = e1.inter(e2); // 2 3 vs 3 4
+        assertEquals(1, inter.size());
+        assertFalse(inter.contains(2));
+        assertTrue(inter.contains(3));
+        assertFalse(inter.contains(4));
+
+    }
+    public void testDif() {
+
+        question1.Ensemble<Integer> e1, e2;
+        e1 = new question1.Ensemble<Integer>();
+        e2 = new question1.Ensemble<Integer>();
+
+        e1.add(2);
+        e1.add(3);
+
+        e2.add(3);
+        e2.add(4);
+
+
+        question1.Ensemble<Integer> dif = e1.diff(e2); // 2 3 vs 3 4
+        assertEquals(1, dif.size());
+        assertTrue(dif.contains(2));
+        assertFalse(dif.contains(3));
+        assertFalse(dif.contains(4));
+
+        question1.Ensemble<Integer> dif2 = e2.diff(e1); // 2 3 vs 3 4
+        assertEquals(1, dif2.size());
+        assertFalse(dif2.contains(2));
+        assertFalse(dif2.contains(3));
+        assertTrue(dif2.contains(4));
+    }
+
+
+    public void testDifSym()
+        {
+
+
+            question1.Ensemble<Integer> e1, e2;
+            e1 = new question1.Ensemble<Integer>();
+            e2 = new question1.Ensemble<Integer>();
+
+            e1.add(2);
+            e1.add(3);
+            e1.add(4);
+            e1.add(5);
+            e1.add(66);
+
+
+            e2.add(4);
+            e2.add(5);
+            e2.add(7);
+            e2.add(6);
+            e2.add(66);
+
+
+            question1.Ensemble<Integer> diffSym = e1.diffSym(e2); // 2 3 vs 3 4
+            assertTrue(diffSym.contains(2));
+            assertTrue(diffSym.contains(3));
+            assertTrue(diffSym.contains(6));
+            assertTrue(diffSym.contains(7));
+            assertFalse(diffSym.contains(4));
+            assertFalse(diffSym.contains(5));
+            assertFalse(diffSym.contains(66));
+
+            assertEquals(4, diffSym.size());
+        }
+    }
+
